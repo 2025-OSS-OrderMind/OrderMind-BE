c260aa9304ead01a76fbee3bc21727b14e70cfdf
parang12 <gimt95513@gmail.comgit config --listgit config --global user.email gimt95513@gmail.com>
2025-11-21 22:43:14 +0900

    chor: FastAPI에서 받는 json파일에 이메일 및 ignore_items추가 (#24)

--- src/server.py
+++ src/server.py
@@ -1,20 +1,12 @@
 from fastapi import FastAPI 
 from pydantic import BaseModel,Field  #구조체 정의를 위해 import
 from fastapi import File, UploadFile, Form 
+from libs.schemas import Item
 import os #파일 및 디렉토리 작업을 위한 os 모듈
 import subprocess 
 import sys
 import uuid #고유 파일 이름 생성을 위한 uuid 모듈
 import threading
-
-""" 구조체 정의 """
-class ItemDetail(BaseModel):
-    name: str                
-    keywords: list[str] | list[None] #키워드 리스트, 기본값은 빈 리스트
-
-class Item(BaseModel):
-    date_range: dict[str, str]  #  예: {"start": "2023-01-01", "end": "2023-01-31"}
-    items : list[ItemDetail] | list[None]  # ItemDetail 객체의 리스트, 기본값은 빈 리스트
 
 app = FastAPI()
 
@@ -94,7 +86,7 @@ async def upload_file(
         threading.Thread(target=stream_reader, args=(process.stdout, "STDOUT"), daemon=True).start()
         threading.Thread(target=stream_reader, args=(process.stderr, "STDERR"), daemon=True).start()
 
-        sys.stdout.flush()
+        #sys.stdout.flush()
         # 즉시 응답 반환 (프로세스는 백그라운드에서 계속 실행)
         return {
             "message": f"{file.filename} 파일 업로드 완료. 백그라운드에서 처리 중입니다.",
